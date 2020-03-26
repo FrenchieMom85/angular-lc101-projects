c0ede49003754f1b6e98a8c99f14b2cc4c04e702
amfzn503 <amfzn503@msn.com>
2020-03-26 14:43:33 -0500

    exerice pt 1

--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['Harry Potter', 'Avengers', 'Star Wars', 'A lot like love'];
 
    constructor() { }
 
